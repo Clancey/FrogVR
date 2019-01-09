5f14ae26a3ae4b5d0944798f01561c68f547c34d
James Clancey <james.clancey@gmail.com>
2019-01-08 20:17:49 -0900

    Added head to  vr player

---
+++ Assets/Scripts/CameraHeadTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHeadTracker : MonoBehaviour
+{
+    public GameObject cameraToFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraToFollow == null)
+            return;
+        this.transform.SetPositionAndRotation(cameraToFollow.transform.position, cameraToFollow.transform.rotation);
+    }
+}
